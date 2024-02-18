b4a1eab0050cdf8abd445d3f603c9c24706b1f28
Rodrigo Schieck <rodrigodh@Rodrigos-MacBook-Pro.local>
2024-02-18 09:21:07 -0300

    sketchybar: looking better.

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# The $NAME variable is passed from sketchybar and holds the name of
-# the item invoking this script:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
-
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
-
+sketchybar --set $NAME \
+  label="$(date '+%a %b %d %Y %-I:%M %p')" \
+  icon="" icon.color=0xffff9cbe
